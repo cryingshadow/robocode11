94608db5e394cf4d8813fbff64066caffd51da4a
fc <30505286+felixcleve@users.noreply.github.com>
2017-07-28 14:50:47 +0200

    second version mit movefactor (forward / backward)

--- src/main/java/de/metro/robocode/fc.java
+++ src/main/java/de/metro/robocode/fc.java
@@ -3,6 +3,7 @@ package de.metro.robocode;
 import robocode.*;
 
 public class fc extends AdvancedRobot {
+	double movefactor = 1;
 	public void run() {
 		//turn heading to 0
 		if (getHeading()<=180) {
@@ -16,18 +17,22 @@ public class fc extends AdvancedRobot {
 
 		// robot main loop
 		while(true) {
-			setAhead(1000);
-			turnGunLeft(180);
-			turnGunRight(180);
+			setAhead(movefactor*1000);
+			turnGunLeft(movefactor*360);
+			//turnGunRight(360);
 		}
 	}
 	public void onScannedRobot(ScannedRobotEvent e) {
 		fire(1);
 	}
 	public void onHitByBullet(HitByBulletEvent e) {
-		back(25);
+		movefactor = movefactor * -1;
 	}
 	public void onHitWall(HitWallEvent e) {
-		turnLeft(90);
+		turnLeft(movefactor*90);
+	}
+	public void onHitRobot(HitRobotEvent e) {
+		turnGunRight(getHeading()-getGunHeading());
+		fire(4);
 	}
 }
