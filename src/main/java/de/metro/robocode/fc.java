2292944fd05adac7127b52ada2253e6f3b52e844
fc <30505286+felixcleve@users.noreply.github.com>
2017-07-28 16:50:59 +0200

    update

--- src/main/java/de/metro/robocode/fc.java
+++ src/main/java/de/metro/robocode/fc.java
@@ -1,9 +1,12 @@
 package de.metro.robocode;
 
 import robocode.*;
+import java.util.Random;
 
 public class fc extends AdvancedRobot {
+{
 	double movefactor = 1;
+	boolean block = false;
 	public void run() {
 		//turn heading to 0
 		if (getHeading()<=180) {
@@ -13,26 +16,43 @@ public class fc extends AdvancedRobot {
 			turnRight(360-getHeading());
 		}
 		//move to ceiling
-		ahead(500);
+		ahead(1000);
 
 		// robot main loop
 		while(true) {
-			setAhead(movefactor*1000);
+		    Random r = new Random(); 
+		    double way = 100.0 + r.nextDouble() * 500.0;
+			setAhead(movefactor*way);
 			turnGunLeft(movefactor*360);
-			//turnGunRight(360);
 		}
 	}
 	public void onScannedRobot(ScannedRobotEvent e) {
-		fire(1);
+		if (e.getDistance()<100) {
+			block=true;
+			double angle = (getHeading()+e.getBearing()-getGunHeading());
+			if (angle<180){
+				turnGunRight(angle);
+			}
+			else {
+				turnGunRight(angle-360);
+			}
+			
+			fire(5);
+			block=false;
+		}
+		else {
+			fire(1);
+		}
+		
 	}
 	public void onHitByBullet(HitByBulletEvent e) {
 		movefactor = movefactor * -1;
-	}
+	} 
 	public void onHitWall(HitWallEvent e) {
 		turnLeft(movefactor*90);
 	}
 	public void onHitRobot(HitRobotEvent e) {
 		turnGunRight(getHeading()-getGunHeading());
-		fire(4);
+		fire(5);
 	}
 }
